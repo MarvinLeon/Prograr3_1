beb05748436f0296b31c962642af7ccc8fbb3abe
Grileo <user@DESKTOP-EIK8A53>
2019-02-20 19:23:27 -0600

    agregacion de beans, tienda, cajeros

---
+++ src/progra3/Pagos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra3;
+
+/**
+ *
+ * @author user
+ */
+public class Pagos {
+    public int pagoID;
+    public int pago;
+    
+}
