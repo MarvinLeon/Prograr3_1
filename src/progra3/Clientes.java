321ff0b3f8e0e3d43a4103078a7c50432029ded5
Grileo <user@DESKTOP-EIK8A53>
2019-02-20 19:00:42 -0600

    modificacion de beans. trabajando nuevo repositorio.

---
+++ src/progra3/Clientes.java
@@ -0,0 +1,123 @@
+package progra3;
+
+/**
+ *
+ * @author user
+ */
+public class Clientes {
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getLimiteCredito() {
+        return limiteCredito;
+    }
+
+    public void setLimiteCredito(String limiteCredito) {
+        this.limiteCredito = limiteCredito;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public String getComentario1() {
+        return comentario1;
+    }
+
+    public void setComentario1(String comentario1) {
+        this.comentario1 = comentario1;
+    }
+
+    public String getComentario2() {
+        return comentario2;
+    }
+
+    public void setComentario2(String comentario2) {
+        this.comentario2 = comentario2;
+    }
+
+    public Clientes(int nit, String empresa, String nombre, String apellido, String telefono, String correo, String fechaInicio, String limiteCredito, boolean activo, String comentario1, String comentario2) {
+        this.nit = nit;
+        this.empresa = empresa;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.fechaInicio = fechaInicio;
+        this.limiteCredito = limiteCredito;
+        this.activo = activo;
+        this.comentario1 = comentario1;
+        this.comentario2 = comentario2;
+    }
+    public int nit;
+    public String empresa;
+    public String nombre;
+    public String apellido;
+    public String telefono;
+    public String correo;
+    public String fechaInicio;
+    public String limiteCredito;
+    public boolean activo;
+    public String comentario1;
+    public String comentario2;
+    
+    
+}
